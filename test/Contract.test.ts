5d9db88de231624fac6e8c8e1555f82033638ea7
Robert Jordan <robert@jordanjr.com>
2018-06-22 14:11:49 -0600

    Added whitelist test.

--- test/Contract.test.ts
+++ test/Contract.test.ts
@@ -3,12 +3,13 @@ import Web3 from 'web3';
 import { getCollateralPoolContractAddressAsync } from '../src/lib/Contract';
 
 // Types
-import { MarketContract } from '@marketprotocol/types';
+import { MarketContract, MarketContractRegistry } from '@marketprotocol/types';
 
 // Testing variables
 const PROVIDER_URL_RINKEBY = 'https://rinkeby.infura.io/cbHh1p8RT4Q6E97F4gVi';
 const MARKET_CONTRACT_ADDRESS = '0x0c6b44155a305bd611492ba3165fbfa6e59681b3';
 const COLLATERAL_POOL_ADDRESS = '0x3bb28ea157d178da4f4b1e194f5b9a9a6c8397a0';
+const REGISTRY_CONTRACT_ADDRESS = '0x4bc60737323fd065d99c726ca2c0fad0d1077a60';
 
 /**
  * Contract
@@ -22,5 +23,14 @@ describe('Contract class', () => {
     const marketContract: MarketContract = new MarketContract(web3, MARKET_CONTRACT_ADDRESS);
     const poolAddress = await marketContract.MARKET_COLLATERAL_POOL_ADDRESS;
     expect(poolAddress).toEqual(COLLATERAL_POOL_ADDRESS);
+  });
+  it('Contract is found in whitelist', async () => {
+    const web3 = new Web3(new Web3.providers.HttpProvider(PROVIDER_URL_RINKEBY));
+    const marketContractRegistry: MarketContractRegistry = new MarketContractRegistry(
+      web3,
+      REGISTRY_CONTRACT_ADDRESS
+    );
+    const whitelist = await marketContractRegistry.getAddressWhiteList;
+    expect(whitelist).toContain(MARKET_CONTRACT_ADDRESS);
   });
 });
