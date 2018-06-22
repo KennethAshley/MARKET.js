72c76ae717569920e9e23e900c9d459bf7e3cdbd
Robert Jordan <robert@jordanjr.com>
2018-06-22 14:12:51 -0600

    Expose order types.

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,10 @@ import { Settlement } from './lib/Settlement';
 import { SimEx } from './lib/SimEx';
 import { Utils } from './lib/Utils';
 import { Market } from './Market';
+import { ECSignature, Order, SignedOrder } from './types/Order';
 
+// export classes
 export { Market, Settlement, SimEx, Utils };
+
+// export types
+export { ECSignature, Order, SignedOrder };
